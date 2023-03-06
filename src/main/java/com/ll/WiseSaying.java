64bc99d269e9394eb305bb7b62f4c6fcf138055f
hojunking96 <hojunking96@gmail.com>
2023-03-06 15:34:53 +0900

    목록 기능 구현, WiseSaying 명언 클래스 구현

---
+++ src/main/java/com/ll/WiseSaying.java
@@ -0,0 +1,13 @@
+package com.ll;
+
+public class WiseSaying {
+    public long id;
+    public String content;
+    public String author;
+
+    public WiseSaying(long id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+}
