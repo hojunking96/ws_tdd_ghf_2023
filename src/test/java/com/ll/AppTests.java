ded102488752acc08f2614b965fafc7e424c0b78
hojunking96 <hojunking96@gmail.com>
2023-03-06 14:38:53 +0900

    종료기능 구현, 올바르지 않은 명령어 처리

--- src/test/java/com/ll/AppTests.java
+++ src/test/java/com/ll/AppTests.java
@@ -1,43 +1,69 @@
 package com.ll;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
-
-import static com.ll.TestUtil.genScanner;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTests {
+    // 테스트유틸 테스트 시작
     @Test
-    public void 테스트() {
-        assertTrue(1 == 1);
-        assertEquals(1, 1);
-    }
-
-    @Test
-    @DisplayName("스캐너에_키보드가_아닌_문자열을_입력으로_설정")
+    @DisplayName("스캐너에 키보드가 아닌 문자열을 입력으로 설정")
     public void t1() {
-        Scanner sc = genScanner("안녕");
-
+        Scanner sc = TestUtil.genScanner("안녕");
         String cmd = sc.nextLine().trim();
-        assertEquals("안녕", cmd);
+        assertThat(cmd).isEqualTo("안녕");
     }
-
     @Test
     @DisplayName("출력을 모니터에 하지 않고 문자열로 얻기")
     public void t2() {
-        //sout 에 대한 화면출력 금지 시작
+        // System.out 에 대한 화면출력 금지 시작
         ByteArrayOutputStream output = TestUtil.setOutToByteArray();
         System.out.print("안녕");
-        //그 동안 출력되지 않던 내용들을 문자열로 반환
+        // 그 동안 출력되지 않던 내용들을 문자열로 반환
         String rs = output.toString();
-
-        //sout에 대한 화면출력 금지 끝
+        // System.out 에 대한 화면출력 금지 끝
         TestUtil.clearSetOutToByteArray(output);
 
-        assertEquals("안녕", rs);
+        assertThat(rs).isEqualTo("안녕");
     }
+    // 테스트유틸 테스트 끝
+
+    // 앱 테스트 시작
+    @Test
+    @DisplayName("프로그램 시작시 타이틀 출력 그리고 종료")
+    public void t3() {
+        Scanner sc = TestUtil.genScanner("종료");
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+        new App(sc).run();
+
+        String rs = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertThat(rs)
+                .contains("== 명언 앱 ==")
+                .contains("명령) ")
+                .contains("프로그램이 종료되었습니다.")
+                .doesNotContain("올바르지 않은 명령입니다.");
+    }
+
+    @Test
+    @DisplayName("잘못된 명령어 입력에 대한 처리")
+    public void t4() {
+        Scanner sc = TestUtil.genScanner("""
+                종료2
+                종료
+                """.stripIndent().trim());
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+        new App(sc).run();
+
+        String rs = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertThat(rs)
+                .contains("올바르지 않은 명령입니다.");
+    }
+    // 앱 테스트 끝
 }
